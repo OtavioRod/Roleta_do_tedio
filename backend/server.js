2ea3d7623c994538a9950df4831e9b6a339c32cc
OtavioRod <otavioamorimalmeida@hotmail.com>
2026-09-18 15:12:41 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.post("/locais", async (req, res) => {
     console.log("Consulta:", consulta);
 
     const resposta = await fetch(
-      "https://overpass.kumi.systems/api/interpreter",
+      "https://overpass.private.coffee/api/interpreter",
       {
         method: "POST",
         headers: {
